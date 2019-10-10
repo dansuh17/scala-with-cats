814fa26d233d6ddce73e8bdbb6d821e98334176e
Dan Suh <kaehops@gmail.com>
2019-10-10 15:51:20 +0900

    4.7.3 exercise

--- src/main/scala/chapter4/chapter4.scala
+++ src/main/scala/chapter4/chapter4.scala
@@ -374,3 +374,37 @@ object WriterMonad {
   // swap the log and result
   val writer6 = writer1.swap
 }
+
+// 4.7.3 Exercise
+object ShowYourWorking {
+  import cats.data.Writer
+  import scala.concurrent._
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration._
+  import cats.syntax.writer._
+  import cats.syntax.applicative._
+  import cats.instances.vector.catsKernelStdMonoidForVector
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  def slowly[A](body: => A): A =
+    try body
+    finally Thread.sleep(100)
+
+  def factorial(n: Int): Logged[Int] = {
+    for {
+      ans <- slowly({
+        if (n == 0) 1.pure[Logged]
+        else {
+          factorial(n - 1).map(_ * n)
+        }
+      })
+      _ <- Vector(s"fact $n $ans").tell
+    } yield ans
+  }
+
+  val Vector((logA, ansA), (logB, ansB)) = Await.result(
+    Future.sequence(Vector(Future(factorial(3).run), Future(factorial(3).run))),
+    5.seconds
+  )
+}
